77669e2ad838c9a92758bfaf963d260c7b7e3292
dartmn9 <0w27Z&KRyXqe>
2018-05-15 07:11:42 -0400

    added user app 2 program file and started code

---
+++ apps/02-guess-number-app/you_try/program.py
@@ -0,0 +1,15 @@
+import random
+
+print('-----------------------------------')
+print('      GUESS THE NUMBER APP')
+print('-----------------------------------')
+print()
+
+guess_text = random.randint(0, 100)
+
+guess = input('Guess a number between 0 and 100: ')
+
+guess_int = int(guess)
+
+
+
