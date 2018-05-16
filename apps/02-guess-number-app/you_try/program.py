8c79a0da3575b7bc3c2f0f60029a6fed58794290
dartmn9 <0w27Z&KRyXqe>
2018-05-15 21:10:48 -0400

    updates to second app.  Added while loop and if, elif, else statements.

--- apps/02-guess-number-app/you_try/program.py
+++ apps/02-guess-number-app/you_try/program.py
@@ -5,11 +5,21 @@ print('      GUESS THE NUMBER APP')
 print('-----------------------------------')
 print()
 
-guess_text = random.randint(0, 100)
+the_number = random.randint(0, 100)
+guess = -99
 
-guess = input('Guess a number between 0 and 100: ')
+while guess != the_number:
 
-guess_int = int(guess)
+    guess_text = input('Guess a number between 0 and 100: ')
+    guess = int(guess_text)
 
+    if guess < the_number:
+        print('Sorry but is LOWER than the number.')
+
+    elif guess > the_number:
+        print('Sorry but is HIGHER than the number.')
+
+    else:
+        print('Congratulations, you guessed correctly!')
 
 
