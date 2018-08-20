93c269789a4180863d49c9a26c34bc540f1b98b0
dartmn9 <0w27Z&KRyXqe>
2018-08-19 20:35:38 -0400

    Updated app 7 with classes and actors.py file

--- apps/07_wizard_battle/you_try/program.py
+++ apps/07_wizard_battle/you_try/program.py
@@ -1,3 +1,9 @@
+import random
+import time
+
+from actors import Wizard, Creature
+
+
 def main():
     print_header()
     game_loop()
@@ -11,18 +17,39 @@ def print_header():
 
 
 def game_loop():
+    creatures = [Creature('Toad', 1), Creature('Tiger', 12), Creature('Bat', 3),
+                 Creature('Dragon', 50), Creature('Evil Wizard', 1000)]
+
+    hero = Wizard('Gandolf', 75)
+
     while True:
+
+        active_creature = random.choice(creatures)
+        print(
+            'A {} of level {} has appeared from a dark and foggy forest...'.format(
+                active_creature.name, active_creature.level))
+        print()
 
         cmd = input('Do you [a]ttack, [r]unaway, or [l]ook around?')
         if cmd == 'a':
-            print('attack')
+            if hero.attack(active_creature):
+                creatures.remove(active_creature)
+            else:
+                print("The wizard runs and hides taking time to recover...")
+                time.sleep(5)
+                print("The wizard returns revitalized!")
         elif cmd == 'r':
-            print('runaway')
+            print('The wizard has become unsure of his power and flees!!!')
         elif cmd == 'l':
-            print('lookaround')
+            print('The wizard {} takes in the surroundings and sees:'.format(
+                hero.name))
+            for c in creatures:
+                print(" *  A {} of level {}".format(c.name, c.level))
         else:
             print("OK, exiting game... bye!")
             break
+
+        print()
 
 
 if __name__ == '__main__':
