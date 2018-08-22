5a491480b463160496c78f6f40ad84f1a2581560
dartmn9 <0w27Z&KRyXqe>
2018-08-22 19:51:05 -0400

    Final app 7 commit

--- apps/07_wizard_battle/you_try/program.py
+++ apps/07_wizard_battle/you_try/program.py
@@ -1,7 +1,7 @@
 import random
 import time
 
-from actors import Wizard, Creature
+from actors import Wizard, Creature, SmallAnimal, Dragon
 
 
 def main():
@@ -17,8 +17,9 @@ def print_header():
 
 
 def game_loop():
-    creatures = [Creature('Toad', 1), Creature('Tiger', 12), Creature('Bat', 3),
-                 Creature('Dragon', 50), Creature('Evil Wizard', 1000)]
+    creatures = [SmallAnimal('Toad', 1), Creature('Tiger', 12),
+        SmallAnimal('Bat', 3), Dragon('Dragon', 50, 75, True),
+        Wizard('Evil Wizard', 1000)]
 
     hero = Wizard('Gandolf', 75)
 
@@ -47,6 +48,10 @@ def game_loop():
                 print(" *  A {} of level {}".format(c.name, c.level))
         else:
             print("OK, exiting game... bye!")
+            break
+
+        if not creatures:
+            print("You've defeated all the creatures!!!")
             break
 
         print()
