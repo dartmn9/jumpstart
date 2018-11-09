9c9a3418baa1c09cf6a81eb6f30701745876e1c7
Dave Riley <dartmn9@yahoo.com>
2018-11-08 21:06:40 -0500

    Updated commit of app 9 to include lambdas and dictionaries.

---
+++ apps/09_real_estate_analyzer/you_try/concept_dicts.py
@@ -0,0 +1,22 @@
+lookup = {}
+lookup = dict()
+lookup = {'age': 42, 'loc': 'Italy'}
+lookup = dict(age=42, loc='Italy')
+
+
+class Wizard:
+    def __init__(self, name, level):
+        self.level = level
+        self.name = name
+
+
+gandolf = Wizard("Gladolf", 42)
+print(gandolf.__dict__)
+
+print(lookup)
+print(lookup['loc'])
+
+lookup['cat'] = 'Fun code demos'
+
+if 'cat' in lookup:
+    print(lookup['cat'])
--- apps/09_real_estate_analyzer/you_try/program.py
+++ apps/09_real_estate_analyzer/you_try/program.py
@@ -1,4 +1,7 @@
+import csv
 import os
+
+from data_types import Purchase
 
 
 def main():
@@ -17,15 +20,64 @@ def print_header():
 
 def get_data_file():
     base_folder = os.path.dirname(__file__)
-    return os.path.join(base_folder, 'data',
+    return os.path.join(base_folder,
                         'SacramentoRealEstateTransactions2008.csv')
 
 
 def load_file(filename):
-    return []
+    with open(filename, 'r', encoding='utf-8') as fin:
+        reader = csv.DictReader(fin)
+        purchases = []
+        for row in reader:
+            p = Purchase.create_from_dict(row)
+            purchases.append(p)
+
+        return purchases
+
+        # header = fin.readline().strip()
+        # reader = csv.reader(fin, delimiter= ',')
+        # for row in reader:
+        #     print(type(row), row)
+        #     beds = row[4]
 
 
-def query_data(data):
+# def load_file_basic(filename):
+#     with open(filename, 'r', encoding='utf-8') as fin:
+#         header = fin.readline().strip()
+#         print('found header: ' + header)
+#
+#         lines = []
+#         for line in fin:
+#             line_data = line.strip().split(',')
+#             bed_count = line_data[4]
+#             lines.append(line_data)
+#
+#         print(lines[:5])
+
+
+# def get_price(p):
+#     return p.price
+
+def query_data(data):  # list[Purchase]
+
+    # if data was sorted by price:
+    # data.sort(key=get_price)
+    data.sort(key=lambda p: p.price)
+
+    # most expensive house?
+    high_purchase = data[-1]
+    print("The most expensive house is ${:,} with {} beds "
+          "and {} baths".format(high_purchase.price, high_purchase.beds,
+                                high_purchase.baths))
+
+    # least expensive house?
+    low_purchase = data[0]
+    print("The least expensive house is ${:,} with {} beds "
+          "and {} baths".format(low_purchase.price, low_purchase.beds,
+                                low_purchase.baths))
+
+    # average price house?
+    # average price of 2 bedroom houses?
     pass
 
 
